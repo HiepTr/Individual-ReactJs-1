b23c203bec11de2315dd5a7f8ef129a1c5ea8a02
HiepTr <118036757+HiepTr@users.noreply.github.com>
2022-11-12 11:55:37 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+const name = 'Joshua Scott';
+const App = () => {
+  return (
+    <div className="App">
+      <h1>You're {name}?</h1>
+     
+    </div>
+  );
+}
+
+export default App;
